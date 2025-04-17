d4848ea6d12cc52d0ccb4975432b9043b5687ef8
Aleksandr <Aleksandr_egorov_07@mail.ru>
2025-04-17 15:07:48 +0400

    Реализовал чтение существующего пользователя и добавление нового.

--- FitnessAppBL/Controller/UserController.cs
+++ FitnessAppBL/Controller/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using FitnessAppBL.Model;
 
@@ -13,42 +15,66 @@ namespace FitnessAppBL.Controller
         /// <summary>
         /// Пользователь приложения.
         /// </summary>
-        public User User { get; }
+        public List<User> Users { get; }
+        public User CurrentUser { get; }
+        public bool IsNewUser { get; } = false;
 
         /// <summary>
         /// Создание нового контроллера.
         /// </summary>
         /// <param name="user"> Имя пользователя. </param>
         /// <exception cref="ArgumentNullException">Пользователь не может быть равен null.</exception>
-        public UserController(string userName,
-                              string genderName,
-                              DateTime birthDate,
-                              double weight,
-                              double height)
+        public UserController(string userName)
         {
-            // TODO: Проверка.
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть путсы или null", nameof(userName));
+            }
 
-            var gender = new Gender(genderName);
-            User = new User(userName, gender, birthDate, weight, height);
+            Users = GetUsersData();
+
+            CurrentUser = Users.SingleOrDefault(u => u.Name == userName);
+
+            if (CurrentUser == null)
+            {
+                CurrentUser = new User(userName);
+                Users.Add(CurrentUser);
+                IsNewUser = true;
+                Save();
+            }
         }
 
         /// <summary>
-        /// Получить данные пользователя.
+        /// Получить сохранённый список пользователей.
         /// </summary>
-        /// <returns> Данные пользователя. </returns>
-        public UserController()
+        /// <returns> Данные пользователей. </returns>
+        private List<User> GetUsersData()
         {
             var formatter = new BinaryFormatter();
 
             using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
-                if (formatter.Deserialize(fs) is User user)
+                if (formatter.Deserialize(fs) is List<User> users)
                 {
-                    User = user;
+                    return users;
                 }
 
-                // TODO: Что делать если пользователя не прочитали?
+                else
+                {
+                    return new List<User>();
+                }
             }
+        }
+
+        public void SetNewUserData(string genderName, DateTime birthDate, double weight = 1, double height = 1)
+        {
+            // Проверка.
+
+            CurrentUser.Gender =  new Gender(genderName);
+            CurrentUser.BirthDate = birthDate;
+            CurrentUser.Weight = weight;
+            CurrentUser.Height = height;
+            Save();
         }
 
         /// <summary>
@@ -60,7 +86,7 @@ namespace FitnessAppBL.Controller
 
             using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
-                formatter.Serialize(fs, User);
+                formatter.Serialize(fs, Users);
             }
         }
     }
