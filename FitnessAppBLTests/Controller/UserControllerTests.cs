0ea3bc8579ab89ba87e357e0f569735a5748329f
Aleksandr <Aleksandr_egorov_07@mail.ru>
2025-04-17 16:25:52 +0400

    Добавил модульные тесты.

---
+++ FitnessAppBLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FitnessAppBL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessAppBL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            // Arrange
+            var userName = Guid.NewGuid().ToString();
+            var birthDate = DateTime.Now.AddYears(-18);
+            var weight = 80;
+            var height = 190;
+            var gender = "man";
+            var controller = new UserController(userName);
+
+            // Act
+            controller.SetNewUserData(gender, birthDate, weight, height);
+            var controller2 = new UserController(userName);
+
+            // Assert
+            Assert.AreEqual(userName, controller2.CurrentUser.Name);
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            // Arrange
+            var userName = Guid.NewGuid().ToString();
+
+            // Act
+            var controller = new UserController(userName);
+
+            // Assert
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+    }
+}
